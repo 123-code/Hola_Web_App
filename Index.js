6414549e1307e4cb6bc4e5473a2c870a03bd9993
123-code <naranjojose256@gmail.com>
2022-03-13 22:48:29 -0500

    Updated Backend

--- Index.js
+++ Index.js
@@ -1,13 +1,78 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+//const MONGO_URI = require(process.env.MONGO_URI);
+const PORT = process.env.PORT || 5000;
 
-
-app.get('/',(req,res)=>{
-  res.send("Hello");
-})
-
-
-app.listen(8000,()=>{
-  console.log("server listening on port 8000!")
+const Qschema = new mongoose.Schema({
+  Nombre: String,
+  Pregunta: String,
 });
+
+
+const DatabaseC = async ()=>{
+
+  try{
+    const client = await mongoose.connect('mongodb://localhost:27017/Preguntas', { useNewUrlParser: true, useUnifiedTopology: true });
+   const Database = client.db('Preguntas');
+    client.close();
+   }catch(err){
+     res.status(500).json({message:err});
+    }}
+     
+   
+app.post("api/pregunta"),async(req,res)=>{
+DatabaseC(async()=>{
+
+  await Database.save(req.body).then(()=>{ 
+    res.status(200);
+  }).catch(err=>{
+    res.status(500).json({message:err});
+    console.info(err);
+  })
+
+})
+    
+}
+
+app.get('api/Pregunta'),async(req,res)=>{
+  DatabaseC = async ()=>{await Database.find((Pregunta,err)=>{
+    if(!err){
+      res.get(200).json(Pregunta);
+    }
+    else if(err){
+      res.status(500);
+    }
+  })}
+
+}
+
+app.post("api/nombre"),async(req,res)=>{
+  DatabaseC = async ()=>{ await Database.save(req.body).then(()=>{
+    res.status(200);
+
+  }).catch(err=>{
+    res.status(500);
+    console.info(err);
+  })}
+ 
+
+}
+
+app.get("api/nombre"),async(req,res)=>{
+  DatabaseC = async ()=>{
+    
+    await Database.find(nombre).then(()=>{
+    res.status*(200);
+  }).catch(err=>{
+    res.status(500);
+    console.info(err);
+
+  })}
+ 
+}
+
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+})
