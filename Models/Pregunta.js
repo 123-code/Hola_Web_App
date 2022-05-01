48482fe89d73d31b604bba64fa6ee7a78278f055
123-code <naranjojose256@gmail.com>
2022-05-01 15:28:38 -0500

    Updated File

--- Models/Pregunta.js
+++ Models/Pregunta.js
@@ -4,12 +4,13 @@ const PreguntaSchema = mongoose.Schema(
     {
     // aqui se puede Cambiar a Many dt
     Nombre:{required:true,type:String},
-    Pregunta:{required:true,type:String,index:{unique:true}},
+    Contenido:{required:true,type:String,index:{unique:true}},
     
 },
 {
 timestamps:true, 
 }
 );
+
 console.info("Worked");
 module.exports = mongoose.model("Pregunta",PreguntaSchema);
---
+++ Models/Producto.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+    Nombre:{reuired:true,type:String},
+    Precio:{required:true,type:Number},
+
+})
+module.exports = mongoose.model("Producto",ProductSchema);
