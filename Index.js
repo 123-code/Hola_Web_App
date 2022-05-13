dcfbe19bcc12b2a6a12146cf16c6e3de1c5a6fde
123-code <naranjojose256@gmail.com>
2022-05-13 14:20:52 -0500

    File Update

--- Index.js
+++ Index.js
@@ -3,26 +3,28 @@ const app = express();
 const mongoose = require("mongoose");
 const url = "mongodb://localhost:27017/Foro";
 const Pregunta = require('./Models/Pregunta');
-
+ 
 //const MONGO_URI = require(process.env.MONGO_URI); 
 const PORT = process.env.PORT || 5008;
 
 
 
 // falta hacer test con postman 
-const DatabaseC = async ()=>{
+const DatabaseC = async (req,res)=>{
 
   try{
+   
     const Database = client.db(url);
-    client.close();
-    return mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+    
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.info("conectado");
    }catch(err){
      res.status(500).json({message:err});
+     console.info('error');
     }}
      
    
-app.post("/api/spregunta"),async(req,res)=>{
+app.post("/api/pregunta"),async(req,res)=>{
   res.send(' guardar pregunta');
   let pregunta = new PreguntaSchema();
   pregunta.Nombre = req.body.Nombre;
@@ -35,6 +37,7 @@ pregunta.save((data,err)=>{
   else{
     console.info(`data saved${data}`);
   }
+  DatabaseC();
 })
     
 }
@@ -45,9 +48,11 @@ app.get('/api/Pregunta'),async(req,res)=>{
   DatabaseC = async ()=>{await Database.find((Pregunta,err)=>{
     if(!err){
       res.get(200).json(Pregunta);
+      console.info("Data found");
     }
     else if(err){
       res.status(500);
+      console.info("Data not found");
     }
     DatabaseC();
   })}
@@ -78,10 +83,11 @@ app.get("api/Nombre"),async(req,res)=>{
 
 app.get('/',(req,res)=>{
     res.send("Main Page");
+    DatabaseC();
 })
 
 app.get('/foro',(req,res)=>{
-    res.send("Foro");
+    res.send('l');
 });
 
 app.get('/material',(req,res)=>{
--- Models/Pregunta.js
+++ Models/Pregunta.js
@@ -5,7 +5,6 @@ const PreguntaSchema = mongoose.Schema(
     // aqui se puede Cambiar a Many dt
     Nombre:{required:true,type:String},
     Contenido:{required:true,type:String,index:{unique:true}},
-    
 },
 {
 timestamps:true, 
